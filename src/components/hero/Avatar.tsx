f319a310143e6b47701cbdc14242ffe81d615111
raheeldurwesh <durweshraheel@gmail.com>
2026-07-30 13:46:20 +0530

    Delete src directory

--- src/components/hero/Avatar.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-// Legacy 3D character avatar component (replaced by AICore 3D Quantum Reactor)
-export default function Avatar() {
-  return null;
-}
--- src/components/hero/GLTFAvatar.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-// Legacy 3D GLTF avatar model loader (replaced by AICore 3D Quantum Reactor)
-export default function GLTFAvatar() {
-  return null;
-}
